fcd91116fdf57082ef06b2d5bd7ae8bec082f04a
leon-joel <nakaue+gh@gmail.com>
2016-04-04 19:27:04 +0900

    EffectiveRuby 項目29 p.114 まで

--- EffectiveRuby/super_forwardable.rb
+++ EffectiveRuby/super_forwardable.rb
@@ -1,4 +1,4 @@
-require 'pp'
+#require 'pp'
 require 'active_support/core_ext/string/strip'
 
 
@@ -121,9 +121,175 @@ p rh.invert
 # p rh["test2"]   # -> KeyError
 
 
+puts "delegateを試す"
+puts "http://magazine.rubyist.net/?0012-BundledLibraries"
+
+puts <<-EOS.strip_heredoc
+
+SimpleDelegator
+=======================================================
+EOS
 
 
+require 'delegate'
+foo = SimpleDelegator.new([])
+foo.push(1)
+foo.push(2)
+puts "foo.size: #{foo.size}"  # -> 2
+
+p foo                         # -> [1, 2]
+p foo.class
+p foo.class.ancestors         # -> [SimpleDelegator, Delegator, #<Module:0x29c9da8>, BasicObject]
+p foo.class.instance_methods
+
+puts "委譲先オブジェクト"
+p foo.__getobj__              # -> [1, 2]
+p foo.__getobj__.class.ancestors        # -> [Array, Enumerable, Object, Kernel, BasicObject]
 
 
-★★★★★★★★★★ delegateも試してみたい ★★★★★★★★★★★★
-http://magazine.rubyist.net/?0012-BundledLibraries
+puts
+puts "RubyDocの例"
+
+foo = Object.new
+def foo.test
+  p 25
+end
+
+foo2 = SimpleDelegator.new(foo)
+foo2.test
+p foo.class.ancestors         # -> [Object, PP::ObjectMixin, Kernel, BasicObject]
+
+                              # ppをrequireしないようにすると、
+                              # -> [Object, Kernel, BasicObject]
+
+p foo2.__getobj__             # -> #<Object:0x2a67500>
+
+puts <<-EOS.strip_heredoc
+
+DelegateClass()
+=======================================================
+EOS
+class ExtArray < DelegateClass(Array)
+  def initialize
+    super([])
+  end
+end
+
+p ExtArray.ancestors          # -> [ExtArray, #<Class:0x2a314e8>, Delegator, #<Module:0x299ded0>, BasicObject]
+a = ExtArray.new
+a.push 25
+p a
+
+p a.methods
+p a.instance_variables
+p a.__getobj__.class
+
+puts <<-EOS.strip_heredoc
+
+inheritedフックを使って、継承禁止クラスを作成する
+EOS
+
+module PreventInheritance
+  class InheritanceError < StandardError; end
+
+  def inherited(child)
+    raise(InheritanceError, "#{child} cannot inherit from #{self}")
+  end
+end
+
+p Array.singleton_methods         # -> [:[], :try_convert]
+
+Array.extend(PreventInheritance)  # extendすることで、モジュールに定義したフックメソッド（インスタンスメソッド）を
+                                  # クラスメソッドとして取り込ませる
+
+p Array.singleton_methods         # -> [:[], :try_convert, :inherited]
+
+begin
+  class BetterArray < Array; end    # -> BetterArray cannot inherit from Array (PreventInheritance::InheritanceError)
+rescue => e
+  puts e
+end
+
+puts <<-EOS
+
+その他のフックメソッド
+==========================================================
+EOS
+
+class InstanceMethodWatcher
+  def self.method_added(m);     puts "added #{self}##{m}"; end
+  def self.method_removed(m);   puts "removed #{self}##{m}"; end
+  def self.method_undefined(m); puts "undefined #{self}##{m}"; end
+
+  def hello; end
+
+  remove_method(:hello)
+
+  def hello; end
+
+  undef_method(:hello)
+
+
+  def self.singleton_method_added(m); puts "added singleton method #{self}::#{m}"; end
+  def self.singleton_method_removed(m); puts "removed singleton method #{self}::#{m}"; end
+  def self.singleton_method_undefined(m); puts "undefined singleton method #{self}::#{m}"; end
+
+  def self.hello; end
+
+  # self.remove_method(:hello)  # これはNG -> private method `remove_method' called for InstanceMethodWatcher:Class (NoMethodError)
+  class << self                 # これはOK。remove_methodは【特異クラスのprivateメソッド】なので、classをOPENしないと呼び出せない。
+    p self.ancestors            # -> [#<Class:InstanceMethodWatcher>, #<Class:Object>, #<Class:BasicObject>, Class, Module, Object, Kernel, BasicObject]
+    remove_method(:hello)
+  end
+
+  p self.ancestors              # -> [InstanceMethodWatcher, Object, Kernel, BasicObject]
+
+  def self.hello; end
+
+  class << self; undef_method(:hello); end
+end
+
+
+puts <<-EOS
+
+inheritedフックを使った「派生クラスを登録する仕組み」
+============================================================
+EOS
+
+class DownloaderBase
+  def self.inherited(subclass)
+    super                         # クラスフックの中にはsuper呼び出しを入れよう（項目29，p.114）
+    puts "derived #{subclass} from #{self}"
+    handlers << subclass
+  end
+
+  def self.handlers
+    @handlers ||= []
+  end
+
+  private_class_method(:handlers)
+end
+
+class SSHDownloader < DownloaderBase; end
+class HTTPDownloader < DownloaderBase; end
+class FtpDownloader < DownloaderBase; end
+class TFtpDownloader < FtpDownloader; end    # 派生クラスの派生クラス ※基底クラスのhandlerには登録されない
+
+p TFtpDownloader.ancestors
+
+
+class TFtpDownloader
+  # p handlers
+end
+
+# p DownloaderBase.class_variables
+# p DownloaderBase.private_instance_methods
+# p DownloaderBase.private_methods
+
+class DownloaderBase
+  puts "#{self} : #{handlers}"
+end
+
+class FtpDownloader
+  puts "#{self} : #{handlers}"
+end
