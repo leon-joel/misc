b0d58df398ddd35f1628e71676ad82b888e34c18
leon-joel <nakaue+gh@gmail.com>
2016-04-13 15:06:35 +0900

    EffectiveRuby 項目37 p.150 まで Minitest Spec

--- EffectiveRuby/test/version_test.rb
+++ EffectiveRuby/test/version_test.rb
@@ -26,3 +26,30 @@ class VersionTest < Minitest::Test
     assert_operator(@v1, :<, @v2)
   end
 end
+
+
+describe "Version" do
+  describe "when parsing" do
+    before do
+      @version = Version.new("10.8.9")
+    end
+
+    it 'should creates 3 integers' do
+      @version.major.must_equal 10
+      @version.minor.must_equal 8
+      @version.patch.must_equal 9
+    end
+  end
+
+  describe "when comparing" do
+    before do
+      @v1  = Version.new('2.1.1')
+      @v2  = Version.new('2.3.0')
+    end
+
+    it 'orders comparing' do
+      @v1.wont_equal @v2
+      @v1.must_be(:<, @v2)
+    end
+  end
+end
